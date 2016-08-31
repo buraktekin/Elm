1580d9c44145c239f69563a2a34c8000e467e2ad
burak <tknbrk@gmail.com>
2016-08-31 16:49:39 +0300

    Add Reverse String example. Update concept is experienced.

---
+++ ReverseString/reverseString.elm
@@ -0,0 +1,37 @@
+import Html exposing (Html, Attribute, div, input, text)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import String
+
+main =
+  App.beginnerProgram {
+    model = model,
+    view = view,
+    update = update
+  }
+
+-- MODEL
+type alias Model = { content : String }
+
+model : Model
+model =
+  Model "" -- Initial value. --
+
+-- UPDATE
+type Msg
+  = Change String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ placeholder "Text to reverse", onInput Change ] []
+    , div [] [ text (String.reverse model.content) ]
+    ]
